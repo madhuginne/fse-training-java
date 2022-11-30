b78f7137606faaa0461aaf52f405fbd6d0ee1dd8
labuser <labuser@FSE.2hvxgt22ikuu5dnjgrnhcfjh1g.rx.internal.cloudapp.net>
2022-11-30 11:21:03 +0000

    Modified Questions in Exam class

--- java-fse-training/src/com/fse/examonconsole/Exam.java
+++ java-fse-training/src/com/fse/examonconsole/Exam.java
@@ -15,18 +15,18 @@ public class Exam {
 		Question question4 = new Question("04", "What do you mean by chained exceptions in Java?",
 				"Exceptions occurred by the VirtualMachineError", "An exception caused by other exceptions",
 				"Exceptions occur in chains with discarding the debugging information", "None of the above", 2);
-		Question question5 = new Question("05", "Which one of the following is not a java key word?", "this", "String",
-				"char", "list", 4);
-		Question question6 = new Question("1", "Which one of the following is not a java key word?", "this", "String",
-				"char", "list", 4);
-		Question question7 = new Question("1", "Which one of the following is not a java key word?", "this", "String",
-				"char", "list", 4);
-		Question question8 = new Question("1", "Which one of the following is not a java key word?", "this", "String",
-				"char", "list", 4);
-		Question question9 = new Question("1", "Which one of the following is not a java key word?", "this", "String",
-				"char", "list", 4);
-		Question question10 = new Question("1", "Which one of the following is not a java key word?", "this", "String",
-				"char", "list", 4);
+		Question question5 = new Question("05", "Which environment variable is used to set the java path?", "JAVA_HOME",
+				"JAVA", "Java", "J2EE_HOME", 1);
+		Question question6 = new Question("1", "What is the extension of compiled java classes?", ".jv", ".java",
+				".jcf", ".class", 4);
+		Question question7 = new Question("1", "Which of the following is a superclass of every class in Java?", "Object Class", "String Class",
+				"Java Class", "Java Lang Class", 1);
+		Question question8 = new Question("1", "Which one of the following is not an access modifier?", "private", "protected",
+				"public", "volatile", 4);
+		Question question9 = new Question("1", "JDK stands for ____.", "Java Deployable Kit", "Java Development Kit",
+				"Java Developers Kit", "None of these", 2);
+		Question question10 = new Question("1", "What is a map in Java?", "Data structure", "Defined in java.util package",
+				"Represented using key-value pairs", "All of the above", 4);
 
 		Question[] questionArray = { question1, question2, question3, question4, question5, question6, question7,
 				question8, question9, question10 };
